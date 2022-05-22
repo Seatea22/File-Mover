4df77db9952f7634ca194ad19d8ef09afbcd4302
Caleb Talbott <calebtalbott22@gmail.com>
2022-05-22 18:06:47 -0400

    ~ Some name changes + Documentation and comments

--- src/main/java/com/dlmanage/dlmanage/FileManageController.java
+++ src/main/java/com/dlmanage/dlmanage/FileManageController.java
@@ -17,43 +17,45 @@ import java.nio.file.*;
 
 public class FileManageController {
 
-    File mainDirectory;
-    File[] fileList;
-    //ArrayList<File> directoryList = new ArrayList<>();
-    HashMap<File,String[]> directoryList = new HashMap<>();
+    File mainDirectory; //File for the source directory
+    File[] fileList; //List of all the files in the source directory.
 
-    String audioPath = "C:\\Users\\caleb\\Downloads\\Audio";
-    String mcPath = "C:\\Users\\caleb\\Downloads\\Minecraft";
-    String zipPath = "C:\\Users\\caleb\\Downloads\\Zips";
-    String picturePath = "C:\\Users\\caleb\\Downloads\\Pictures";
-    String videoPath = "C:\\Users\\caleb\\Downloads\\Videos";
-    String pdfPath = "C:\\Users\\caleb\\Downloads\\PDFs";
+    //Map of all destination files and their corresponding extensions.
+    HashMap<File,String[]> destinationMap = new HashMap<>();
 
     @FXML
-    private ListView<File> listView;
-    public ListView<File> dListView;
+    private ListView<File> listView; //List view for source directory
+    public ListView<File> dListView; //List view for destination directories.
 
-    public Label nameLabel;
-    public Label lastMod;
-    public Label fileType;
+    public Label nameLabel; //Label for source directory selected file names
+    public Label lastMod; //Label for source directory selected file's last modification
+    public Label fileType; //Label for source directory selected file types
 
-    public Label nameLabelDest;
-    public Label lastModDest;
-    public TextField destExtInput;
+    public Label nameLabelDest; //Label for destination selected directory file names
+    public Label lastModDest; //Label for destination selected directory file's last modification
+
+    public TextField destExtInput; //Text field for input of file extensions
+    //Button to submit file extensions to be assigned to a destination directory in the destination map
     public Button destExtButton;
-    public Text extensions;
+    public Text extensions; //Label to list selected destination file extensions
 
     @FXML
-    public MenuItem openButton;
-    public MenuItem openDestButton;
-    public Button moveButton;
+    public MenuItem openButton; //Menu button to open source directory
+    public MenuItem openDestButton; //Menu button to open destination directory
+    public Button moveButton; //Button to start the process of moving files
 
+    /**
+     * Opens directory chooser and loads file.
+     */
     public void openFile(ActionEvent event) {
         DirectoryChooser fileChooser = new DirectoryChooser();
         mainDirectory = fileChooser.showDialog(null);
         loadDirectory();
     }
 
+    /**
+     * Loads all the files from the source directory into the list view.
+     */
     public void loadDirectory() {
         if (mainDirectory != null) {
             fileList = mainDirectory.listFiles();
@@ -67,6 +69,10 @@ public class FileManageController {
         }
     }
 
+    /**
+     * Opens directory chooser, and if directory is chosen, it loads the directory into the destination map.  If config
+     * file is included, it will set the extensions of the directory to those in the config file.
+     */
     public void openDestFile(ActionEvent actionEvent) {
         DirectoryChooser fileChooser = new DirectoryChooser();
         File newDir = fileChooser.showDialog(null);
@@ -75,17 +81,22 @@ public class FileManageController {
             for (File file : files) {
                 if (file.getName().equals("config.txt")) {
                     String[] args = checkConfig(file);
-                    directoryList.put(newDir, args);
+                    destinationMap.put(newDir, args);
                 }
             }
         } else {
-            directoryList.put(newDir, null);
+            destinationMap.put(newDir, null);
         }
 
         dListView.getItems().clear();
-        directoryList.forEach((k, v) -> dListView.getItems().add(k));
+        destinationMap.forEach((k, v) -> dListView.getItems().add(k));
     }
 
+    /**
+     * Checks the first line of the config file and returns an array of all the extensions.
+     * @param file File to be checked
+     * @return String array of file extensions
+     */
     public String[] checkConfig(File file) {
         String[] args = {};
         try {
@@ -101,11 +112,16 @@ public class FileManageController {
         return args;
     }
 
-    public void moveFiles(ActionEvent actionEvent) throws IOException {
+    /**
+     * When the move button is pressed, all the files in the source directory will have their extensions checked
+     * and compared to the destination map. Those files will then be moved if the file extension is to be moved.
+     * Source directory list view is then refreshed.
+     */
+    public void moveButtonPressed(ActionEvent actionEvent) throws IOException {
         for (File file : fileList) {
-            System.out.println(file.getAbsolutePath());
+            //System.out.println(file.getAbsolutePath());
             String ext = getExtension(file.getName());
-            System.out.println(ext);
+            //System.out.println(ext);
 
             /*
             if (ext.equals("mp3") || ext.equals("wav") || ext.equals("ogg")) {
@@ -128,11 +144,11 @@ public class FileManageController {
             }
             */
 
-            directoryList.forEach((k, v) ->
+            destinationMap.forEach((k, v) -> //k = file, v = extension array
             {
                 if (k != null) {
                     for (int i = 0; i < v.length; i++) {
-                        if (ext.equals(v[i])) {
+                        if (ext.equals(v[i])) { //compares the extensions of file to those of the destination's ext array
                             moveFile(file.getAbsolutePath(), k.getAbsolutePath() + "\\" + file.getName());
                         }
                     }
@@ -144,6 +160,11 @@ public class FileManageController {
         }
     }
 
+    /**
+     * Returns the file extension of a given file name.
+     * @param filename String of file name
+     * @return String of file extension
+     */
     public String getExtension(String filename) {
         String extension = "";
         int i = filename.lastIndexOf('.');
@@ -151,6 +172,11 @@ public class FileManageController {
         return extension;
     }
 
+    /**
+     * Moves a file from source to target.
+     * @param sourcePath String of the source path
+     * @param targetPath String of the target path
+     */
     public void moveFile(String sourcePath, String targetPath) {
         try {
             Files.move(Paths.get(sourcePath), Paths.get(targetPath), StandardCopyOption.REPLACE_EXISTING);
@@ -159,7 +185,11 @@ public class FileManageController {
         }
     }
 
-    public void listSelected(MouseEvent fileEditEvent) {
+    /**
+     * When an item in the source directory list view is selected, the information for that item will be displayed
+     * on the right.
+     */
+    public void listSelected(MouseEvent mouseEvent) {
         if (!listView.getItems().isEmpty() && listView.getSelectionModel().getSelectedIndex() != -1) {
             int index = listView.getSelectionModel().getSelectedIndex();
             nameLabel.setText(fileList[index].getName());
@@ -171,20 +201,30 @@ public class FileManageController {
         }
     }
 
+
+    /**
+     * When an item in the destination list view is selected, the information for that item will be displayed
+     * on the right.
+     */
     public void destListSelected(MouseEvent mouseEvent) {
         if (!dListView.getItems().isEmpty() && dListView.getSelectionModel().getSelectedIndex() != -1) {
             File selectedItem = dListView.getSelectionModel().getSelectedItem();
             nameLabelDest.setText(selectedItem.getName());
             lastModDest.setText(String.valueOf(selectedItem.lastModified()));
-            extensions.setText(Arrays.toString(directoryList.get(selectedItem)));
+            extensions.setText(Arrays.toString(destinationMap.get(selectedItem)));
         }
     }
 
+
+    /**
+     * When the submit button is pressed, the contents inside the text box above the button, will be designated
+     * to the selected directory in the destination list view.
+     */
     public void destinationExtSubmit(ActionEvent actionEvent) {
         if (!dListView.getItems().isEmpty() && !listView.getItems().isEmpty()) {
             File selectedItem = dListView.getSelectionModel().getSelectedItem();
             String[] extInput = destExtInput.getText().split("\\s+");
-            directoryList.put(selectedItem, extInput);
+            destinationMap.put(selectedItem, extInput);
         }
     }
 }
